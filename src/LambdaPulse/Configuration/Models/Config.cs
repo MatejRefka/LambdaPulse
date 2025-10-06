638372071fadb32890f45bf63973fc97e0712056
Matej Refka <Matej.Refka@hd-clinical.com>
2025-10-06 20:02:45 +0100

    Exception handler config setup

--- src/LambdaPulse/Configuration/Models/Config.cs
+++ src/LambdaPulse/Configuration/Models/Config.cs
@@ -12,6 +12,16 @@
         public required int Port { get; init; }
 
         public int BackLog { get; init; } = 100;
+
+        public required MiddlewareConfig MiddlewareConfig { get; init; }
     }
 
+    public sealed class MiddlewareConfig
+    {
+        public required string MaxControlDataSizeBytes { get; init; }
+        public required string MaxHeaderCount { get; init; }
+        public required string MaxHeaderSizeBytes { get; init; }
+        public required string MaxBodySizeBytes { get; init; }
+        public required string RequestReadTimeoutLimit { get; init; }
+    }
 }
