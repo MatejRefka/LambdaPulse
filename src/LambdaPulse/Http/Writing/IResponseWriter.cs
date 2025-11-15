384624bed7b9507fb69394be61f7788818619b61
Matej Refka <matejrefka@googlemail.com>
2025-11-15 17:34:10 +0000

    Response writing implementation

--- src/LambdaPulse/Http/Writing/IResponseWriter.cs
+++ src/LambdaPulse/Http/Writing/IResponseWriter.cs
@@ -5,5 +5,5 @@ namespace LambdaPulse.Services.Http;
 
 public interface IResponseWriter
 {
-    public Task WriteHttpResponse(NetworkStream networkStream, WebResponse response);
+    public Task WriteHttpResponse(NetworkStream networkStream, WebResponse webResponse);
 }
--- src/LambdaPulse/Http/Writing/ResponseWriter.cs
+++ src/LambdaPulse/Http/Writing/ResponseWriter.cs
@@ -1,4 +1,5 @@
 ﻿using LambdaPulse.Services.Http.Models;
+using System.Globalization;
 using System.Net.Sockets;
 using System.Text;
 
@@ -6,14 +7,42 @@ namespace LambdaPulse.Services.Http;
 
 public sealed class ResponseWriter : IResponseWriter
 {
-    public async Task WriteHttpResponse(NetworkStream networkStream, WebResponse response)
+    public async Task WriteHttpResponse(NetworkStream networkStream, WebResponse webResponse)
     {
-        var responseOK = "HTTP/1.1 200 OK\r\n" +
-                       "Content-Type: text/plain\r\n" +
-                       "Content-Length: 2\r\n" +
-                       "\r\n" +
-                       "OK";
-        var responseBytes = Encoding.UTF8.GetBytes(responseOK);
-        await networkStream.WriteAsync(responseBytes);
+        var responseStatusLine = $"HTTP/1.1 {webResponse.StatusCode} {webResponse.ResponsePhrase}\r\n";
+
+        var bodyBytes = webResponse.Body == null ? Array.Empty<byte>() : Encoding.UTF8.GetBytes(webResponse.Body);
+
+        //recommended response headers for http/1.1
+        if (!webResponse.Headers.ContainsKey("Content-Length"))
+        {
+            webResponse.Headers["Content-Length"] = bodyBytes.Length > 0 ? bodyBytes.Length.ToString(CultureInfo.InvariantCulture) : "0";
+        }
+        if (!webResponse.Headers.ContainsKey("Content-Type"))
+        {
+            webResponse.Headers["Content-Type"] = "text/plain; charset=utf-8";
+        }
+        if (!webResponse.Headers.ContainsKey("Connection"))
+        {
+            webResponse.Headers["Connection"] = webResponse.Headers["Connection"] = "close";
+        }
+
+        //parse WebResponse headers into string
+        string headersBlock = responseStatusLine;
+        foreach (var header in webResponse.Headers)
+        {
+            headersBlock += $"{header.Key}: {header.Value}\r\n";
+        }
+        headersBlock += "\r\n";
+
+        //encode headers
+        var headerBytes = Encoding.ASCII.GetBytes(headersBlock);
+
+        //send headers and body
+        await networkStream.WriteAsync(headerBytes);
+        if (bodyBytes.Length > 0)
+        {
+            await networkStream.WriteAsync(bodyBytes);
+        }
     }
 }
