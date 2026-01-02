1c15671407087f0ffa9d95b377a218c32e7c973d
Matej Refka <matejrefka@googlemail.com>
2026-01-02 23:18:20 +0000

    Moved internal server DI build from Program.cs into Builder class

---
+++ src/LambdaPulse/Hosting/ServerBuilder.cs
@@ -0,0 +1,40 @@
+﻿using LambdaPulse.Configuration;
+using LambdaPulse.DI;
+using LambdaPulse.Services;
+using LambdaPulse.Services.Http;
+using LambdaPulse.Services.Http.Routing;
+using LambdaPulse.Services.Http.State;
+
+namespace LambdaPulse;
+
+public static class ServerBuilder
+{
+    public static WebServer Build(EndpointRegistry endpointRegistry)
+    {
+        //register services
+        var container = new DependencyContainer();
+        container.AddSingleton<IConfigProvider, ConfigProvider>();
+
+        container.AddSingleton<IConnectionListener, ConnectionListener>();
+
+        container.AddSingleton<IClientHandler, ClientHandler>();
+
+        container.AddSingleton<IRequestReader, RequestReader>();
+        container.AddSingleton<IRequestParser, RequestParser>();
+        container.AddSingleton<IResponseWriter, ResponseWriter>();
+
+        container.AddSingleton<ISessionStore, InMemorySessionStore>();
+
+        container.AddSingleton(endpointRegistry);
+
+        container.AddSingleton<WebServer>();
+
+        //need the resolver itself to resolve MW dependencies
+        var resolver = new DependencyResolver(container);
+        container.AddSingleton(resolver);
+
+        var webServer = resolver.GetService<WebServer>();
+
+        return webServer ?? throw new InvalidOperationException("Cannot construct WebServer");
+    }
+}
