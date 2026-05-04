2915af05a0b381f46090445aafa13a076d0c7f9e
Matej Refka <Matej.Refka@hd-clinical.com>
2026-05-04 14:52:28 +0100

    Project reference fixes + spelling fixes

--- tests/LambdaPulse.Tests/Core/MockEngineLogger.cs
+++ tests/LambdaPulse.Tests/Core/MockEngineLogger.cs
@@ -4,5 +4,5 @@ namespace LambdaPulse.Tests.Core;
 
 public class MockEngineLogger : IEngineLogger
 {
-    public void Log(LogLevel logLevel, string message, Exception? exception = null) { }
+    public void Log(LogLevel logLevel, string source, string message, Exception? exception = null) { }
 }
