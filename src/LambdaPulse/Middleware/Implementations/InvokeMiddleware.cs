2b52434fb121844bf4b045b49f5623d86bfbf801
Matej Refka <matejrefka@googlemail.com>
2026-05-18 20:05:01 +0100

    Invoke and termination mw added telemetry

--- src/LambdaPulse/Middleware/Implementations/InvokeMiddleware.cs
+++ src/LambdaPulse/Middleware/Implementations/InvokeMiddleware.cs
@@ -1,6 +1,8 @@
-﻿using LambdaPulse.Engine.Http.Abstractions;
+﻿using LambdaPulse.Engine.Features.Logging;
+using LambdaPulse.Engine.Http.Abstractions;
 
 namespace LambdaPulse.Engine.Middleware.Implementations;
+
 /// <summary>
 /// Executes user code that's mapped to the requested endpoint.
 /// </summary>
@@ -14,13 +16,18 @@ internal sealed class InvokeMiddleware : MiddlewareBase
 
     public override async Task Invoke(WebContext webContext, CancellationToken cancellationToken = default)
     {
+        var downstreamStart = DateTimeOffset.UtcNow;
+        var logs = new List<string>();
+
         if (webContext.Endpoint != null)
         {
             //user application code is invoked here
             await webContext.Endpoint.ApplicationFunction(webContext, cancellationToken);
-            return;
+            logs.Add("Endpoint invoked.");
         }
 
+        RecordTelemetry(webContext, FlowDirection.Downstream, ExecutionEvent.Success, downstreamStart);
         await _nextFunction(webContext, cancellationToken);
+        RecordTelemetry(webContext, FlowDirection.Upstream, ExecutionEvent.Success, DateTime.UtcNow);
     }
 }
--- src/LambdaPulse/Middleware/Implementations/TerminationMiddleware.cs
+++ src/LambdaPulse/Middleware/Implementations/TerminationMiddleware.cs
@@ -1,8 +1,12 @@
-﻿using LambdaPulse.Engine.Http.Abstractions;
+﻿using LambdaPulse.Engine.Features.Logging;
+using LambdaPulse.Engine.Http.Abstractions;
 using LambdaPulse.Engine.Shared.Extensions;
 
 namespace LambdaPulse.Engine.Middleware.Implementations;
 
+/// <summary>
+/// Sets 404 Not Found response when no response body has been written.
+/// </summary>
 internal sealed class TerminationMiddleware : MiddlewareBase
 {
     protected override string MiddlewareName => "Termination";
@@ -13,11 +17,17 @@ internal sealed class TerminationMiddleware : MiddlewareBase
 
     public override async Task Invoke(WebContext webContext, CancellationToken cancellationToken = default)
     {
-        if (!webContext.WebResponse.HasBody)
+        var downstreamStart = DateTimeOffset.UtcNow;
+        var logs = new List<string>();
+
+        if (webContext.WebResponse.StatusCode == null)
         {
             webContext.WebResponse.StatusCode = 404;
             webContext.WebResponse.ResponsePhrase = "Not Found";
             await webContext.WebResponse.WriteStringToBody("Not Found.", cancellationToken);
+            logs.Add("No response status code was set. Set 404 Not Found.");
         }
+
+        RecordTelemetry(webContext, FlowDirection.Downstream, ExecutionEvent.Success, downstreamStart, logs);
     }
 }
