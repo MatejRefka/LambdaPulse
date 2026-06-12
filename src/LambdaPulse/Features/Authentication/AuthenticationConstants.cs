fbc3e65a6a6f95c1f6b2f2635431a282c83b0cab
Matej Refka <matejrefka@googlemail.com>
2026-06-12 19:14:36 +0100

    Login and register endpoint implementation

--- src/LambdaPulse/Features/Authentication/AuthenticationConstants.cs
+++ src/LambdaPulse/Features/Authentication/AuthenticationConstants.cs
@@ -3,4 +3,6 @@
 public static class AuthenticationDefaults
 {
     public const string UserIdSessionKey = "auth.user_id";
+
+    public const string DummyPasswordHash = "$2a$12$C.L6Jk3FzCgeuH5Y5iQW6OFJBCm5ulCkMZKM1J7ZIErS4YK9b.I6G";
 }
