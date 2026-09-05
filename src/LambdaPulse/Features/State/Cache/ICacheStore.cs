86bad707371afd5bc2e8a15f6552e4476c25ea99
Matej Refka <matejrefka@googlemail.com>
2026-09-05 21:34:33 +0100

    Adjusted method definition and method name for ICacheStore.

--- src/LambdaPulse/Features/State/Cache/ICacheStore.cs
+++ src/LambdaPulse/Features/State/Cache/ICacheStore.cs
@@ -4,5 +4,8 @@ public interface ICacheStore
 {
     Task<CachedResponse?> GetCachedResponse(string key, CancellationToken cancellationToken = default);
 
-    Task<bool> SetCachedResponse(string key, CachedResponse cachedResponse, CancellationToken cancellationToken = default);
+    /// <summary>
+    /// Saves the given cached response
+    /// </summary>
+    Task<bool> SaveCachedResponse(string key, CachedResponse cachedResponse, CancellationToken cancellationToken = default);
 }
--- src/LambdaPulse/Features/State/Cache/InMemoryCacheStore.cs
+++ src/LambdaPulse/Features/State/Cache/InMemoryCacheStore.cs
@@ -23,9 +23,9 @@ internal sealed class InMemoryCacheStore : ICacheStore
         return Task.FromResult<CachedResponse?>(cachedResponse);
     }
 
-    public Task<bool> SetCachedResponse(string key, CachedResponse cachedResponse, CancellationToken cancellationToken = default)
+    public Task SaveCachedResponse(string key, CachedResponse cachedResponse, CancellationToken cancellationToken = default)
     {
         _cachedResponses[key] = cachedResponse;
-        return Task.FromResult(true);
+        return Task.CompletedTask;
     }
 }
