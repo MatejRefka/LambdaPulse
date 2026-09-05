5856499f1b2ce4aa50e48d5cc319cc3b0e351f21
Matej Refka <matejrefka@googlemail.com>
2026-09-05 21:36:30 +0100

    Adjusted method definition and method name for ICacheStore.

--- src/LambdaPulse/Features/State/Cache/ICacheStore.cs
+++ src/LambdaPulse/Features/State/Cache/ICacheStore.cs
@@ -7,5 +7,5 @@ public interface ICacheStore
     /// <summary>
     /// Saves the given cached response
     /// </summary>
-    Task<bool> SaveCachedResponse(string key, CachedResponse cachedResponse, CancellationToken cancellationToken = default);
+    Task SaveCachedResponse(string key, CachedResponse cachedResponse, CancellationToken cancellationToken = default);
 }
